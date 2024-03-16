341531696ac13843c8191e58e1a160c170f62e78
ROSHAN BAIG MOGAL <96071713+mogalroshanbaig@users.noreply.github.com>
2024-03-17 02:35:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+document.querySelector('a[href="#resume"]').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default behavior (scrolling to the section)
+    window.open('C:/Users/mogal/OneDrive/Desktop/Portfolio/resume/ROSHANBAIG_RESUME.pdf', '_blank');
+});
+ document.querySelectorAll('nav ul li a').forEach(link => {
+            link.addEventListener('click', event => {
+                event.preventDefault();
+                const href = link.getAttribute('href');
+                const offsetTop = document.querySelector(href).offsetTop;
+                window.scrollTo({
+                    top: offsetTop,
+                    behavior: 'smooth'
+                });
+            });
+        });
+    

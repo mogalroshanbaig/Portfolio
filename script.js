6d230605c81a836ee52b9a8b3d40df2ba0329952
ROSHAN BAIG MOGAL <96071713+mogalroshanbaig@users.noreply.github.com>
2024-03-17 13:11:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 document.querySelector('a[href="#resume"]').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default behavior (scrolling to the section)
-    window.open('C:/Users/mogal/OneDrive/Desktop/Portfolio/resume/ROSHANBAIG_RESUME.pdf', '_blank');
+    window.open('resume/ROSHANBAIG_RESUME.pdf', '_blank');
 });
  document.querySelectorAll('nav ul li a').forEach(link => {
             link.addEventListener('click', event => {
